047867607ac30ebbf36f3411c2964c99ea457ac7
BenterkiaAyoub <benterkia.ayoub1@gmail.com>
2024-01-21 03:20:11 -0800

    last commit

--- console.py
+++ console.py
@@ -289,4 +289,3 @@ class HBNBCommand(cmd.Cmd):
 
 if __name__ == '__main__':
     HBNBCommand().cmdloop()
-
---
+++ models/amenity.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""
+for the amenity class.
+"""
+from models.base_model import BaseModel
+
+
+class Amenity(BaseModel):
+    """
+    for represent an Amenity.
+
+    Attributes:
+        name (str): name of the Amenity.
+    """
+
+    name = ""
